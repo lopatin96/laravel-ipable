64de70e5e541a4e44f4462ae4c37c444deb358a0
vladyslavlopatin <ilopatin3@gmail.com>
2024-06-15 22:22:50 +0200

    request()?->ip()

--- src/Traits/HasIp.php
+++ src/Traits/HasIp.php
@@ -8,14 +8,14 @@ trait HasIp
 {
     public function scopeWithIpAddress($query, ?string $ipAddress = null): void
     {
-        $query->where('ip', $ipAddress ?? request()->ip());
+        $query->where('ip', $ipAddress ?? request()?->ip());
     }
 
     public static function bootHasIp(): void
     {
         static::creating(static function (Model $model) {
             if (is_null($model->ip_address)) {
-                $model->ip_address = request()->ip();
+                $model->ip_address = request()?->ip();
             }
         });
     }
