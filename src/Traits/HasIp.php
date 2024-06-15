dac93505b38640b87b4474b164bad905e46d8f3b
vladyslavlopatin <ilopatin3@gmail.com>
2024-06-15 22:08:12 +0200

    rename ip column to ip_address and add a scope scopeWithIpAddress

--- src/Traits/HasIp.php
+++ src/Traits/HasIp.php
@@ -3,16 +3,19 @@
 namespace Atin\LaravelIpable\Traits;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Request;
-use Illuminate\Support\Str;
 
 trait HasIp
 {
+    public function scopeWithIpAddress($query, ?string $ipAddress = null): void
+    {
+        $query->where('ip', $ipAddress ?? request()->ip());
+    }
+
     public static function bootHasIp(): void
     {
         static::creating(static function (Model $model) {
-            if (is_null($model->ip)) {
-                $model->ip = Request::ip();
+            if (is_null($model->ip_address)) {
+                $model->ip_address = request()->ip();
             }
         });
     }
